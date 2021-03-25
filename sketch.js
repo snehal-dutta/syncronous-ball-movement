559ab87ca8a357482da4b8ab710aac69e806b961
snehal-dutta <75314110+snehal-dutta@users.noreply.github.com>
2021-03-25 15:31:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var database;
+var canvas, backgroundImage;
+var gameState = 0;
+var playerCount
+var form, game, player;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  canvas = createCanvas(500,500);
+
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  
+}
